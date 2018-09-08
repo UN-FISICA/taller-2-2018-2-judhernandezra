074466a6e9f47047b3e9b4ccfb8bae5e7a98bcd8
Juan David Hernadez Ramirez <judhernandezra@unal.edu.co>
2018-09-08 09:31:47 -0500

    Se modifica ejercicio 4 para que no se superpongan los polígonos

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -19,14 +19,14 @@ for j in range(np+1):
 	i=-j+1
 	while -j<i<j:
 		turtle.penup()
-		turtle.goto(i*15,200-(30*(j-1)))
+		turtle.goto(i*n*7,200-(15*n*(j-1)))
 		turtle.rt(90+(ang/2))
 		turtle.fd(10)
 		turtle.lt(90+(ang/2))
 		i=i+2
 		for count in range(n):
 			turtle.pendown()
-			turtle.fd(15)
+			turtle.fd(25)
 			turtle.lt(ang)
 			
 
