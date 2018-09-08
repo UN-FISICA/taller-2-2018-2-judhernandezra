f380a16bafa3bad694621e294e2f7e4b14708229
Juan David Hernadez Ramirez <judhernandezra@unal.edu.co>
2018-09-08 09:28:19 -0500

    Ejercicio 4 completo, polígonos con lados constantes

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -4,7 +4,29 @@
 import math
 import turtle
 
+n= input("Número de lados del polígono ")
+np= input("Número de filas de la pirámide ")
+
+ang=360/n 
+
+turtle.penup()
 turtle.lt(90)
 turtle.fd(200)
+turtle.rt(90)
+turtle.pendown()
 
+for j in range(np+1):
+	i=-j+1
+	while -j<i<j:
+		turtle.penup()
+		turtle.goto(i*15,200-(30*(j-1)))
+		turtle.rt(90+(ang/2))
+		turtle.fd(10)
+		turtle.lt(90+(ang/2))
+		i=i+2
+		for count in range(n):
+			turtle.pendown()
+			turtle.fd(15)
+			turtle.lt(ang)
+			
 
