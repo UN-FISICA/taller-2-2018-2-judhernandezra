ae55f5193b89a441b188e4d8e0c4e39e330317e9
Juan David Hernadez Ramirez <judhernandezra@unal.edu.co>
2018-09-07 19:39:37 -0500

    add Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+import math
+import turtle 
+x=0
+y=10
+n=input ("Digite el número de lados del polígono ")
+k=0
+if (n%2==0):
+    while (k<=n):
+        print x,y
+        x=10*math.cos((math.pi/n)+(2*math.pi*k/n))
+        y=10*math.sin((math.pi/n)+(2*math.pi*k/n))
+        k=k+1
+else:
+    while (k<=n):
+        print x,y
+        x=10*math.cos((math.pi/2)+(2*math.pi*k/n))
+        y=10*math.sin((math.pi/2)+(2*math.pi*k/n))
+        k=k+1
+        
+
+        
